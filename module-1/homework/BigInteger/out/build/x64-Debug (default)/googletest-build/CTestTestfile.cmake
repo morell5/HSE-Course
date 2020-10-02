502b95b2f4b347852617132c9de67de1cf748fed
Dan <79198736000@mail.ru>
2020-10-02 23:04:32 +0300

    Trash deleted

--- module-1/homework/BigInteger/out/build/x64-Debug (default)/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: B:/Documents/HSE/HSE-Course/module-1/homework/BigInteger/out/build/x64-Debug (default)/googletest-src
-# Build directory: B:/Documents/HSE/HSE-Course/module-1/homework/BigInteger/out/build/x64-Debug (default)/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")

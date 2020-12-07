fd69ebe6d9ee5335c8d3ea8dd3ccae21c4069cc1
L0u1Za <nigoluiza@mail.ru>
2020-12-07 21:40:20 +0300

    fixed.

--- module-1/homework/TypeList/typelist/append.h
+++ module-1/homework/TypeList/typelist/append.h
@@ -10,14 +10,14 @@ struct Append<NullType, NullType> {
 	typedef NullType NewTypeList;
 };
 
-template<typename H, typename T> // TypeList & Null
-struct Append<TypeList<H, T>, NullType> {
-	typedef TypeList<H, T> NewTypeList;
-};
-
 template<typename NewType> // Null & T
 struct Append<NullType, NewType> {
 	typedef TypeList<NewType, NullType> NewTypeList;
+};
+
+template<typename H, typename T> // TypeList & Null
+struct Append<TypeList<H, T>, NullType> {
+	typedef TypeList<H, T> NewTypeList;
 };
 
 template<typename H, typename NewType> // HeadList & T
@@ -25,9 +25,9 @@ struct Append<TypeList<H, NullType>, NewType> {
 	typedef TypeList<H, TypeList<NewType, NullType>> NewTypeList;
 };
 
-template<typename H1, typename T1, typename T> // TypeList & T
-struct Append<TypeList<H1, T1>, T> {
-	typedef Append<H1, typename Append<T1, T>::NewTypeList> NewTypeList;
+template<typename H, typename T, typename NewType> // TypeList & T
+struct Append<TypeList<H, T>, NewType> {
+	typedef TypeList<H, typename Append<T, NewType>::NewTypeList> NewTypeList;
 };
 
 template<typename H1, typename T1, typename H2, typename T2>  // TypeList & TypeList

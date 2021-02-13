693af30ac94b9b2b25ad706d740d390a6bde7a71
morell <vorobyev.dv@phystech.edu>
2021-02-13 18:08:42 +0300

    homework: Variant

---
+++ module-1/homework/Variant/task.md
@@ -0,0 +1,10 @@
+# Variant
+
+## Задание
+
+[`Variant`](https://en.cppreference.com/w/cpp/utility/variant) - обощение типа **union**
+
+Реализовать функции:
+* [`get`](https://en.cppreference.com/w/cpp/utility/variant/get) - получение значения в variant по индексу
+* [`get`](https://en.cppreference.com/w/cpp/utility/variant/get) - получение значения в variant по типу
+* [`operator=`](https://en.cppreference.com/w/cpp/utility/variant/operator%3D) - оператор присваивания значения
---
+++ module-1/homework/Variant/variant.h
@@ -0,0 +1,45 @@
+#include <cstdlib>
+
+#pragma once
+
+template<typename... Types>
+class variant;
+
+template<std::size_t Idx, typename _Tp>
+struct variant_alternative;
+
+template<std::size_t Idx, typename T>
+using variant_alternative_t = typename variant_alternative<Idx, T>::type;
+
+template<std::size_t Idx, class... _Types>
+struct variant_alternative<Idx, variant<_Types...>> {
+  using type =  // Your code goes here
+};
+
+template<typename... Types>
+class variant {
+
+    public:
+    
+    // Special member functions
+    constexpr variant() noexcept;
+    
+    template<typename T>
+    variant& operator=( T&& t ) noexcept;
+    
+    private:
+    // Your code goes here
+};
+
+// Non-member functions
+template<std::size_t I, typename... Types>
+constexpr variant_alternative_t<I, variant<Types...>>& get(variant<Types...>& v);
+
+template<std::size_t I, typename... Types>
+constexpr variant_alternative_t<I, variant<Types...>>&& get(variant<Types...>&& v);
+
+template<typename T, typename... Types>
+constexpr T& get(variant<Types...>& v);
+
+template<typename T, typename... Types>
+constexpr T&& get( variant<Types...>&& v );

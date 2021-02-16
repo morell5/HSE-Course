61dc47e7336e5780be21427c6965dcc1cdeaa7be
ChrisBenua <kpbenua@edu.hse.ru>
2021-02-16 14:52:07 +0300

    added optional

--- module-1/homework/Optional/tests.cpp
+++ module-1/homework/Optional/tests.cpp
@@ -1,42 +1,43 @@
 #include <cmath>
 #include <string>
 
-#include <optional>
+//#include <optional>
+#include "optional.h"
 
 #include "gtest/gtest.h"
 
 TEST(ValueOR, Test1) {
-  std::optional<std::string> opt("Hello world");
+  task::optional<std::string> opt("Hello world");
   ASSERT_EQ(opt.value_or("empty"), "Hello world");
 }
 
 TEST(ValueOR, Test2) {
-  std::optional<std::string> opt;
+    task::optional<std::string> opt;
   ASSERT_EQ(opt.value_or("empty"), "empty");
 }
 
 TEST(HasValue, Test1) {
-    std::optional<std::string> opt("Hello world");
+    task::optional<std::string> opt("Hello world");
     ASSERT_TRUE(opt.has_value());
 }
 
 TEST(Reset, Test1) {
-    std::optional<std::string> opt("Hello world");
+    task::optional<std::string> opt("Hello world");
     opt.reset();
     ASSERT_FALSE(opt.has_value());
 }
 
 TEST(ConversionToBool, Test1) {
-    std::optional<std::string> opt("Hello world");
+    task::optional<std::string> opt("Hello world");
     ASSERT_TRUE(opt);
 }
 
 TEST(ArrowOperator, Test1) {
-    std::optional<std::string> opt("Hello world");
+    task::optional<std::string> opt("Hello world");
     ASSERT_EQ(std::string(opt->c_str()), "Hello world");
 }
 
 TEST(IndirectionOperator, Test1) {
-    std::optional<int> opt(1);
+    task::optional<int> opt(1);
     ASSERT_EQ(*opt, 1);
 }

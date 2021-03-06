6870e6aa645bb9060766fe344b0e4b2ab8464160
ChrisBenua <kpbenua@edu.hse.ru>
2021-03-06 14:22:41 +0300

    fix

--- module-1/homework/Variant/tests.cpp
+++ module-1/homework/Variant/tests.cpp
@@ -20,20 +20,20 @@ TEST(Get, Test1_1) {
 }
 
 TEST(Get, Test2) {
-    task::variant<int, double, std::string> v;
+    variant<int, double, std::string> v;
     v = 12.0;
     ASSERT_NEAR(get<double>(v), 12.0, 1e-5);
 }
 
 
 TEST(Get, Test3) {
-    task::variant<int, double, std::string> v;
+    variant<int, double, std::string> v;
     v = "Hello world";
     ASSERT_EQ(get<2>(v), "Hello world");
 }
 
 TEST(Get, Test4) {
-    task::variant<int, double, std::string> v;
+    variant<int, double, std::string> v;
     v = 12.0;
     ASSERT_NEAR(get<1>(v), 12.0, 1e-5);
 }

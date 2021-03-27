80fdcc7a95537a1941d669770cc231bd27057ccd
morell5 <morell@DESKTOP-K5VODHO.localdomain>
2021-03-27 19:09:27 +0300

    homework: Allocator

--- module-1/homework/Allocator/tests.cpp
+++ module-1/homework/Allocator/tests.cpp
@@ -1,215 +1,219 @@
 #include <algorithm>
 #include <list>
-#include <string>
 #include <random>
+#include <string>
 
+#include "gtest/gtest.h"
 #include "src/allocator/allocator.h"
 #include "src/list/list.h"
 
-#include "gtest/gtest.h"
-
 TEST(CopyAssignment, Test) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
-    task::list<std::string, CustomAllocator<std::string>> expected;
-    
+    task::List<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual_copy;
+    std::list<std::string, CustomAllocator<std::string>> expected;
+    std::list<std::string, CustomAllocator<std::string>> expected_copy;
+
     for (std::size_t i = 0; i < 10; i++) {
-        expected.push_back("hello");
+        actual_copy.PushBack("hello");
+        expected_copy.push_back("hello");
     }
 
-    actual = expected;
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    actual = actual_copy;
+    expected = expected_copy;
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(MoveAssignment, Test1) {
-    std::list<std::string> l1;
-    std::list<std::string> l2;
+    task::List<std::string> l1;
+    task::List<std::string> l2;
     std::list<std::string> l3;
     std::list<std::string> l4;
 
-    l1.push_back("hello");
+    l1.PushBack("hello");
     l2 = std::move(l1);
     l3.push_back("hello");
     l4 = std::move(l3);
-    
-    ASSERT_TRUE(std::equal(l1.begin(), l1.end(), l3.begin(), l3.end()));
-    ASSERT_TRUE(std::equal(l2.begin(), l2.end(), l4.begin(), l4.end()));
+
+    ASSERT_TRUE(std::equal(l1.Begin(), l1.End(), l3.begin(), l3.end()));
+    ASSERT_TRUE(std::equal(l2.Begin(), l2.End(), l4.begin(), l4.end()));
 }
 
 TEST(Front, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
-    actual.push_back("hello");
+
+    actual.PushBack("hello");
     expected.push_back("hello");
 
-    actual.push_back("hello");
+    actual.PushBack("hello");
     expected.push_back("hello");
 
-    actual.push_back("hello");
+    actual.PushBack("hello");
     expected.push_back("hello");
 
-    actual.clear();
+    actual.Clear();
     expected.clear();
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(Clear, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.push_back("hello");
+        actual.PushBack("hello");
         expected.push_back("hello");
     }
 
-    actual.clear();
+    actual.Clear();
     expected.clear();
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(Swap, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.push_back("hello");
+        actual.PushBack("hello");
         expected.push_back("hello");
     }
 
-    task::list<std::string, CustomAllocator<std::string>> actual2;
+    task::List<std::string, CustomAllocator<std::string>> actual2;
     std::list<std::string, CustomAllocator<std::string>> expected2;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual2.push_back("world");
+        actual2.PushBack("world");
         expected2.push_back("world");
     }
 
-    actual.swap(actual2);
+    actual.Swap(actual2);
     expected.swap(expected2);
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual2.Begin(), actual2.End(), expected2.begin(), expected2.end()));
 }
 
 TEST(PushBack, Test) {
     std::vector<std::string> actual_v(10, "hello");
     std::vector<std::string> expected_v(10, "hello");
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.push_back(std::move(actual_v[i]));
+        actual.PushBack(std::move(actual_v[i]));
         expected.push_back(std::move(expected_v[i]));
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(EmplaceBack, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.emplace_back("hello");
+        actual.EmplaceBack("hello");
         expected.emplace_back("hello");
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(PopBack, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.emplace_back("hello");
+        actual.EmplaceBack("hello");
         expected.emplace_back("hello");
     }
 
     for (std::size_t i = 0; i < 5; i++) {
-        actual.pop_back();
+        actual.PopBack();
         expected.pop_back();
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(PushFront, Test1) {
     std::vector<std::string> actual_v(10, "hello");
     std::vector<std::string> expected_v(10, "hello");
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.push_front(actual_v[i]);
+        actual.PushFront(actual_v[i]);
         expected.push_front(expected_v[i]);
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(PushFront, Test2) {
     std::vector<std::string> actual_v(10, "hello");
     std::vector<std::string> expected_v(10, "hello");
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.push_front(std::move(actual_v[i]));
+        actual.PushFront(std::move(actual_v[i]));
         expected.push_front(std::move(expected_v[i]));
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
-}   
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
+}
 
 TEST(EmplaceFront, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.emplace_front("hello");
+        actual.EmplaceFront("hello");
         expected.emplace_front("hello");
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(PopFront, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 10; i++) {
-        actual.emplace_back("hello");
+        actual.EmplaceBack("hello");
         expected.emplace_back("hello");
     }
 
     for (std::size_t i = 0; i < 5; i++) {
-        actual.pop_front();
+        actual.PopFront();
         expected.pop_front();
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(Resize, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
-    actual.resize(2);
-    ASSERT_EQ(actual.size(), 2);
+    task::List<std::string, CustomAllocator<std::string>> actual;
+    actual.Resize(2);
+    ASSERT_EQ(actual.Size(), 2);
 }
 
 TEST(Remove, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
-    actual.push_back("hello");
-    actual.push_back("world");
-    actual.push_back("hello");
-    actual.remove("hello");
+    task::List<std::string, CustomAllocator<std::string>> actual;
+    actual.PushBack("hello");
+    actual.PushBack("world");
+    actual.PushBack("hello");
+    actual.Remove("hello");
     std::list<std::string, CustomAllocator<std::string>> expected;
     expected.push_back("world");
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(Unique, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
-    actual.push_back("hello");
-    actual.push_back("hello");
-    actual.push_back("world");
-    actual.push_back("world");
-    actual.unique();
+    task::List<std::string, CustomAllocator<std::string>> actual;
+    actual.PushBack("hello");
+    actual.PushBack("hello");
+    actual.PushBack("world");
+    actual.PushBack("world");
+    actual.Unique();
     std::list<std::string, CustomAllocator<std::string>> expected;
     expected.push_back("hello");
     expected.push_back("world");
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(Sort, Test1) {
@@ -217,42 +221,42 @@ TEST(Sort, Test1) {
     std::mt19937 random_engine(random_device());
     std::uniform_int_distribution<int> distribution(1, 100);
 
-    task::list<int> actual;
+    task::List<int> actual;
     std::list<int> expected;
     for (size_t i = 0; i < 100; ++i) {
         int value = distribution(random_engine);
-        actual.push_back(value);
+        actual.PushBack(value);
         expected.push_back(value);
     }
-    actual.sort();
+    actual.Sort();
     expected.sort();
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 TEST(Mixed, Test1) {
-    task::list<std::string, CustomAllocator<std::string>> actual;
+    task::List<std::string, CustomAllocator<std::string>> actual;
     std::list<std::string, CustomAllocator<std::string>> expected;
-    
+
     for (std::size_t i = 0; i < 5; i++) {
-        actual.push_back("hello");
+        actual.PushBack("hello");
         expected.push_back("hello");
     }
 
     for (std::size_t i = 0; i < 3; i++) {
-        actual.pop_front();
+        actual.PopFront();
         expected.pop_front();
     }
 
     for (std::size_t i = 0; i < 5; i++) {
-        actual.push_front("world");
+        actual.PushFront("world");
         expected.push_front("world");
     }
 
     for (std::size_t i = 0; i < 3; i++) {
-        actual.pop_back();
+        actual.PopBack();
         expected.pop_back();
     }
-    ASSERT_TRUE(std::equal(actual.begin(), actual.end(), expected.begin(), expected.end()));
+    ASSERT_TRUE(std::equal(actual.Begin(), actual.End(), expected.begin(), expected.end()));
 }
 
 int main(int argc, char** argv) {

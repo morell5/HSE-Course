9c9185d72c56898b7eaba9bbccb88740dbf40f71
CardinalPrefect <rumpel135@gmail.com>
2021-06-15 18:53:34 +0300

    type traits solution

--- module-1/homework/TypeTraits/type_traits/is_nothrow_move_constructible.h
+++ module-1/homework/TypeTraits/type_traits/is_nothrow_move_constructible.h
@@ -9,15 +9,31 @@
 template <bool, bool, typename T, typename... Args>
 struct LibCppIsNoThrowConstructible;
 
-// LibCppIsNoThrowConstructible - partial specializations
-// Your code goes here
-// LibCppIsNoThrowConstructible - partial specializations
+template <typename T, typename... Args>
+struct LibCppIsNoThrowConstructible<true, false, T, Args...> {
+    using type = std::integral_constant<bool, noexcept(T(Declval<Args>()...))>;
+};
+
+template <typename T, typename Arg>
+struct LibCppIsNoThrowConstructible<true, true, T, Arg> {
+    using type =
+        std::integral_constant<bool,
+                               noexcept(IsConstructibleHelper::ImplicitCast<T>(Declval<Arg>()))>;
+};
+
+template <bool IsReference, typename T, typename... Args>
+struct LibCppIsNoThrowConstructible<false, IsReference, T, Args...> {
+    using type = std::false_type;
+};
 
 template <typename T, typename... Args>
-struct IsNoThrowConstructible : // Your code goes here {...}
+struct IsNoThrowConstructible
+    : LibCppIsNoThrowConstructible<IsConstructible<T, Args...>::value, std::is_reference_v<T>, T,
+                                   Args...>::type {};
 
-template <typename T, std::size_t N>
-struct IsNoThrowConstructible<T[N]> : // Your code goes here {...}
+template <typename T, size_t N>
+struct IsNoThrowConstructible<T[N]>
+    : LibCppIsNoThrowConstructible<IsConstructible<T>::value, std::is_reference_v<T>, T>::type {};
 
 template <typename T>
-struct IsNoThrowMoveConstructible : // Your code goes here {...}
+struct IsNoThrowMoveConstructible : IsNoThrowConstructible<T, T&&> {};
--- module-1/homework/TypeTraits/type_traits/utility.h
+++ module-1/homework/TypeTraits/type_traits/utility.h
@@ -4,29 +4,88 @@
 #include <utility>
 
 template <typename T>
-struct Uncvref {
-    // Your code goes here
+struct RemoveConst {
+    using type = T;
 };
 
 template <typename T>
-using uncvref_t = // Your code goes here
+struct RemoveConst<T const> {
+    using type = T;
+};
+
+template <typename T>
+using remove_const_t = typename RemoveConst<T>::type;
+
+template <typename T>
+struct RemoveVolatile {
+    using type = T;
+};
+
+template <typename T>
+struct RemoveVolatile<T volatile> {
+    using type = T;
+};
+
+template <typename T>
+using remove_volatile_t = typename RemoveVolatile<T>::type;
+
+template <typename T>
+struct RemoveReference {
+    using type = T;
+};
+
+template <typename T>
+struct RemoveReference<T&> {
+    using type = T;
+};
+
+template <typename T>
+struct RemoveReference<T&&> {
+    using type = T;
+};
+
+template <typename T>
+using remove_reference_t = typename RemoveReference<T>::type;
+
+template <typename T>
+struct Uncv {
+    using type = remove_const_t<remove_volatile_t<T>>;
+};
+
+template <typename T>
+using uncv_t = typename Uncv<T>::type;
+
+template <typename T>
+struct Uncvref {
+    using type = uncv_t<remove_reference_t<T>>;
+};
+
+template <typename T>
+using uncvref_t = typename Uncvref<T>::type;
 
 template <typename T>
 struct AddConst {
-    using type = // Your code goes here
+    using type = const T;
 };
 
 template <typename T>
-using add_const_t = // Your code goes here
+using add_const_t = typename AddConst<T>::type;
 
 template <typename T>
-struct AddLvalueReference : // Your code goes here
+struct AddLvalueReference {
+    using type = T&;
+};
 
 template <typename T>
-struct AddRvalueReference : // Your code goes here
+struct AddRvalueReference {
+    using type = T&&;
+};
 
 template <typename T>
-using add_lvalue_reference_t = // Your code goes here
+using add_lvalue_reference_t = typename AddLvalueReference<T>::type;
 
 template <typename T>
-using add_rvalue_reference_t = // Your code goes here
+using add_rvalue_reference_t = typename AddRvalueReference<T>::type;
+
+template <typename T>
+add_rvalue_reference_t<T> Declval() noexcept;
